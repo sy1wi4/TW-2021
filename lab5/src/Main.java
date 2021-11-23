4a9d9f201475b85acd6852890300da23ca2d0d9e
Sylwia Marek <sylwia.marek13@gmail.com>
2021-11-23 17:28:03 +0100

    add lab6

--- lab5/src/Main.java
+++ lab5/src/Main.java
@@ -3,17 +3,17 @@ public class Main {
         // 8-core CPU
 
         // segment mode
-        new Mandelbrot(1, 200).setVisible(true);
+//        new Mandelbrot(1, 200).setVisible(true);
         new Mandelbrot(8, 200).setVisible(true);
-        new Mandelbrot(16, 200).setVisible(true);
-        new Mandelbrot(32, 200).setVisible(true);
-
-        new Mandelbrot(50, 50).setVisible(true);
-        new Mandelbrot(50, 500).setVisible(true);
-
-        // pixel mode
-        new Mandelbrot(10).setVisible(true);
-        new Mandelbrot(16).setVisible(true);
-        new Mandelbrot(50).setVisible(true);
+//        new Mandelbrot(16, 200).setVisible(true);
+//        new Mandelbrot(32, 200).setVisible(true);
+//
+//        new Mandelbrot(50, 50).setVisible(true);
+//        new Mandelbrot(50, 500).setVisible(true);
+//
+//        // pixel mode
+//        new Mandelbrot(10).setVisible(true);
+//        new Mandelbrot(16).setVisible(true);
+//        new Mandelbrot(50).setVisible(true);
     }
 }
---
+++ lab6/src/zad1b.js
@@ -0,0 +1,35 @@
+let async = require("async")
+function printAsync(s, cb) {
+    var delay = Math.floor((Math.random() * 1000) + 500);
+    setTimeout(function () {
+        console.log(s);
+        if (cb) cb();
+    }, delay);
+}
+
+function task1(cb) {
+    printAsync("1", function () {
+        task2(cb);
+    });
+}
+
+function task2(cb) {
+    printAsync("2", function () {
+        task3(cb);
+    });
+}
+
+function task3(cb) {
+    printAsync("3", cb);
+}
+
+function loop(n) {
+    let tasksArr = []
+    for (let i = 0; i < n; i++) {
+        tasksArr.push(task1)
+    }
+    async.waterfall(tasksArr, function () {
+        console.log("Done!");
+    });
+}
+loop(4)
